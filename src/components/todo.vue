2efd172b63ab32ddae5f8782e3dbf611cafbdbe9
aswani <ashwani.excellencetechnologies@info>
2019-12-24 13:12:30 +0530

    complete ready for review

--- src/components/todo.vue
+++ src/components/todo.vue
@@ -22,7 +22,9 @@
           </v-container>
         </v-card-text>
         <v-card-actions>
-          <v-spacer></v-spacer>
+          <v-card-text class="red--text">
+            <p>{{error}}</p>
+          </v-card-text>
           <v-btn color="blue darken-1" text @click="close">Close</v-btn>
           <v-btn color="blue darken-1" text @click="save">Save</v-btn>
         </v-card-actions>
@@ -30,32 +32,33 @@
     </v-dialog>
     <v-card class="mx-8">
       <v-card-title class="blue-grey lighten-1 white--text">Task To Do :</v-card-title>
-        <div class="d-flex">
-          <div class="py-5" v-for="(task,index) in tasks" :key="index">
-              <v-card class="my-5 mx-5">
-                <v-list-item class="d-flex justify-space-between font-weight-bold title">
-                  {{ task.task }}
-                  <v-checkbox class="mt-5 ml-3" v-model="form.check" @change="onclick"></v-checkbox>
-                </v-list-item>
-                <v-list-item v-if="task.complete"><p class="red--text">Completed</p></v-list-item>
-                <v-list-item>
-                  <v-list-item-content>Date : {{task.date}}</v-list-item-content>
-                </v-list-item>
-                <v-list-item>
-                  <v-list-item-content>Time : {{task.time}}</v-list-item-content>
-                </v-list-item>
-                <v-list-item class="d-flex justify-space-between">
-                  <div>
-                    <a class="float-left" @click="edit(task,index)">Edit</a>
-                  </div>
-                  <div>
-                    <a class="float-left" @click="del(index)">Delete</a>
-                  </div>
-                </v-list-item>
-              </v-card>
-            </div>
+      <div class="d-flex">
+        <div class="py-5" v-for="(task,index) in tasks" :key="index">
+          <v-card class="my-5 mx-5">
+            <v-list-item class="d-flex justify-space-between font-weight-bold title">
+              {{ task.task }}
+              <v-checkbox class="mt-5 ml-3" v-model="task.check" @change="onclick"></v-checkbox>
+            </v-list-item>
+            <v-list-item v-if="task.check">
+              <p class="red--text">Completed</p>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-content>Date : {{task.date}}</v-list-item-content>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-content>Time : {{task.time}}</v-list-item-content>
+            </v-list-item>
+            <v-list-item class="d-flex justify-space-between">
+              <div>
+                <a class="float-left" @click="edit(task,index)">Edit</a>
+              </div>
+              <div>
+                <a class="float-left" @click="del(index)">Delete</a>
+              </div>
+            </v-list-item>
+          </v-card>
         </div>
-            
+      </div>
     </v-card>
   </div>
 </template>
@@ -65,34 +68,44 @@ export default {
   data: () => {
     return {
       dialog: false,
-      form: { task: "", date: "", time: "" ,check:""},
+      form: { task: "", date: "", time: "", check: false },
       val: {},
       tasks: [],
       editIndex: false,
+      error: ""
     };
   },
   methods: {
     save() {
-      if (this.editIndex == true) {
-        this.val = {
-          task: this.form.task,
-          date: this.form.date,
-          time: this.form.time,
-          check: this.form.check
-        };
-        this.tasks.splice(this.indexval, 1, this.val);
-        this.dialog = false;
-        this.clear();
+      if (this.form.task == "") {
+        this.error = "* Task Required";
+      } else if (this.form.date == "") {
+        this.error = "* Date Required";
+      } else if (this.form.time == "") {
+        this.error = "* Time Required";
       } else {
-        this.val = {
-          task: this.form.task,
-          date: this.form.date,
-          time: this.form.time,
-          check: this.form.check
-        };
-        this.tasks.push(this.val);
-        this.dialog = false;
-        this.clear();
+        if (this.editIndex == true) {
+          this.val = {
+            task: this.form.task,
+            date: this.form.date,
+            time: this.form.time,
+            check: this.form.check
+          };
+          this.tasks.splice(this.indexval, 1, this.val);
+          this.dialog = false;
+          this.editIndex = false;
+          this.clear();
+        } else {
+          this.val = {
+            task: this.form.task,
+            date: this.form.date,
+            time: this.form.time,
+            check: this.form.check
+          };
+          this.tasks.push(this.val);
+          this.dialog = false;
+          this.clear();
+        }
       }
     },
     close() {
@@ -107,21 +120,13 @@ export default {
       this.form.task = task.task;
       this.form.date = task.date;
       this.form.time = task.time;
+      this.form.check = task.check;
       this.editIndex = true;
       this.indexval = index;
     },
     clear() {
-      this.form = { task: "", date: "", time: "", check: "" };
-    },
-    onclick(task){
-      if(task.check == true){
-        this.form.check= false;
-        //eslint-disable-next-line
-      console.log("true");
-      }else{
-        //eslint-disable-next-line
-        console.log("false");
-      }
+      this.form = { task: "", date: "", time: "", check: false };
+      this.error = "";
     }
   }
 };
